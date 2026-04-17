abbe94d04f03d568c3d71d73d050fbeb3adebb5a
kjo35861 <kkuu1044@gmail.com>
2026-04-17 17:52:29 +0900

    4.17/ServletFilterInLoginSystem

--- src/main/java/com/example/servletstudy/login/UserController.java
+++ src/main/java/com/example/servletstudy/login/UserController.java
@@ -1,18 +1,33 @@
 package com.example.servletstudy.login;
 
-import javax.naming.Context;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/api/users")
 public class UserController extends HttpServlet {
 
+    private UserService userService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        UserRepository userRepository = new UserRepository(config.getServletContext());
+        userService = new UserService(userRepository);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        new UserRepository(req.getServletContext());
+        // 사용자 전체 조회
+        List<User> users = userService.getAll();   // 받은 모든 사용자 리스트 응답
+        ResponseEntity.builder()
+                .status(200)
+                .body(users)
+                .build()
+                .response(resp);
     }
 }
--- src/main/java/com/example/servletstudy/login/UserService.java
+++ src/main/java/com/example/servletstudy/login/UserService.java
@@ -1,4 +1,9 @@
 package com.example.servletstudy.login;
 
+import java.util.List;
+
 public class UserService {
+    public List<User> getAll() {
+
+    }
 }
